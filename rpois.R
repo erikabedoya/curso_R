bf9d547ae0971998e385c6b802359dbce64e1fb4
Erika Bedoya <erikal.bedoya@gmail.com>
2016-09-22 09:50:23 -0300

    rpoison

---
+++ rpois.R
@@ -0,0 +1,18 @@
+x <- rpois(1, lambda = 8)
+if (x >= 10) {
+  print("x es más grande o igual que 10")
+}
+x
+set.seed(10)
+x <- rpois(1, lambda = 8)
+if (x >= 10) {
+  print("x es más grande o igual que 10")
+}
+x
+if (x > 10) {print("x es mayor a 10")
+print (paste("x es:", x "))} else if (x > 5){print("x es mayor a 5")} else { print("x es menor que 5")}
+
+x<- "dos"
+switch
+gapminder-FiveYearData <- read.csv("data/gapminder-FiveYearData.csv")
+gapminder-FiveYearData
